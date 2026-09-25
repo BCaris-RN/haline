646019e1a16ce8fbcbd18e505f3544a71bede9e8
Brandon W. Caris <brand0n.car1s@protonmail.com>
2026-09-24 16:48:11 -1000

    Add support email and Pages privacy policy

---
+++ docs/index.md
@@ -0,0 +1,10 @@
+---
+title: Haline
+permalink: /
+---
+
+# Haline
+
+Release and policy documents for Haline.
+
+- [Privacy Policy](./privacy)
--- lib/paywallConfig.ts
+++ lib/paywallConfig.ts
@@ -1,3 +1,3 @@
 export const TERMS_URL = 'https://www.apple.com/legal/internet-services/itunes/dev/stdeula/';
 export const PRIVACY_URL = 'https://bcaris-rn.github.io/haline/privacy';
-export const CONTACT_EMAIL = '<<BRANDON_FILLS_IN>>';
+export const CONTACT_EMAIL = 'haline.support@protonmail.com';
